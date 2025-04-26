bc25e3cc03e1adce131252e6410dd2729a89eaf7
Ali salehi <parkour.runner@gmail.com>
2025-04-26 18:45:11 +0300

    missed file to push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import CreatePost from "@/components/CreatePost";
+import { currentUser } from "@clerk/nextjs/server";
+
 export default async function Home() {
+  const user = await currentUser();
+
   return (
-    <div className="m-4">
-      <h1>Home</h1>
+    <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
+      <div className="lg:col-span-6">{user ? <CreatePost /> : null}</div>
     </div>
   );
 }
