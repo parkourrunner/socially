36752395eab8125b7412a18eb465d37890e6c0a2
Ali salehi <parkour.runner@gmail.com>
2025-04-26 20:00:07 +0300

    posts, comments, like

--- src/actions/user.action.ts
+++ src/actions/user.action.ts
@@ -9,7 +9,7 @@ export async function syncUser() {
     const { userId } = await auth();
     const user = await currentUser();
 
-    if (!user || !userId) return;
+    if (!userId || !user) return;
 
     const existingUser = await prisma.user.findUnique({
       where: {
@@ -23,12 +23,12 @@ export async function syncUser() {
       data: {
         clerkId: userId,
         name: `${user.firstName || ""} ${user.lastName || ""}`,
-        username:
-          user.username ?? user.emailAddresses[0].emailAddress.split("@")[0],
+        username: user.username ?? user.emailAddresses[0].emailAddress.split("@")[0],
         email: user.emailAddresses[0].emailAddress,
         image: user.imageUrl,
       },
     });
+
     return dbUser;
   } catch (error) {
     console.log("Error in syncUser", error);
@@ -54,8 +54,10 @@ export async function getUserByClerkId(clerkId: string) {
 
 export async function getDbUserId() {
   const { userId: clerkId } = await auth();
-  if (!clerkId) throw new Error("Unauthorized");
+  if (!clerkId) return null;
+
   const user = await getUserByClerkId(clerkId);
+
   if (!user) throw new Error("User not found");
 
   return user.id;
@@ -71,11 +73,7 @@ export async function getRandomUsers() {
     const randomUsers = await prisma.user.findMany({
       where: {
         AND: [
-          {
-            NOT: {
-              id: userId,
-            }
-          },
+          { NOT: { id: userId } },
           {
             NOT: {
               followers: {
