4825db9f5c6f87b2024e6d5ea97607d6b78154ef
Ali salehi <parkour.runner@gmail.com>
2025-04-28 08:33:48 +0300

    uploadthing added

--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -9,6 +9,7 @@ import { ImageIcon, Loader2Icon, SendIcon } from "lucide-react";
 import { Button } from "./ui/button";
 import { createPost } from "@/actions/post.action";
 import toast from "react-hot-toast";
+import ImageUpload from "./ImageUpload";
 
 function CreatePost() {
   const { user } = useUser();
@@ -56,7 +57,18 @@ function CreatePost() {
             />
           </div>
 
-          {/* Image upload */}
+          {(showImageUpload || imageUrl) && (
+            <div className="border rounded-lg p-4">
+              <ImageUpload
+                endpoint="postImage"
+                value={imageUrl}
+                onChange={(url) => {
+                  setImageUrl(url);
+                  if (!url) setShowImageUpload(false);
+                }}
+              />
+            </div>
+          )}
 
           <div className="flex items-center justify-between border-t pt-4">
             <div className="flex space-x-2">
